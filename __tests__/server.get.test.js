1e1bb4a68cb7e4687b0b0849d818e1d94eda216c
Aghogho <monorienaghogho@gmail.com>
2025-04-12 00:03:12 +0100

    Finished tests

--- __tests__/server.get.test.js
+++ __tests__/server.get.test.js
@@ -23,11 +23,118 @@ describe("Test GET routes", () => {
             headers: headers
         });
 
-        console(res.data);
-        console(res);
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data.length).toBe(50);
+    });
+
+    test("respond to the GET /transaction/:id", async () => {
+
+        const res = await axios.get(`${baseURL}/transactions/5ca4bbc1a2dd94ee58161d6f`, {
+            headers: headers
+        });
 
         expect(res.status).toBe(200);
         expect(res.headers["content-type"]).toMatch("application/json");
-        // expect(res.data)
-    })
+        expect(res.data).toMatchObject({
+            account_id: 183400,
+            transaction_count: 1,
+            transactions: expect.arrayContaining([
+                expect.objectContaining({
+                    amount: 4908,
+                    transaction_code: "buy"
+                })
+            ])
+        });
+    });
+
+    test("respond to the GET /states", async () => {
+
+        const res = await axios.get(`${baseURL}/states`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data.length).toBe(50);
+    });
+
+    test("respond to the GET /states/:id", async () => {
+
+        const res = await axios.get(`${baseURL}/states/67e863090dca3d52466096d8`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data).toEqual({
+            "_id": "67e863090dca3d52466096d8",
+            "isoCode": "AR",
+            "name": "Arkansas"
+          });
+    });
+
+    test("respond to GET /accounts", async () => {
+
+        const res = await axios(`${baseURL}/accounts`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data.length).toBe(1746);
+    });
+
+    test("respond to GET /accounts/:id", async () => {
+
+        const res = await axios.get(`${baseURL}/accounts/5ca4bbc7a2dd94ee5816238c`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data).toMatchObject({
+            account_id: 371138,
+            limit: 9000,
+            products: expect.arrayContaining([
+                "Derivatives",
+                "InvestmentStock"
+            ])
+        });
+    });
+
+    test("respond to GET /customers", async () => {
+
+        const res = await axios.get(`${baseURL}/customers`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data.length).toBe(500);
+    });
+
+
+    test("respond to GET customers/:id", async () => {
+
+        res = await axios(`${baseURL}/customers/5ca4bbcea2dd94ee58162a68`, {
+            headers: headers
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toMatch("application/json");
+        expect(res.data).toMatchObject({
+            username: "fmiller",
+            name: "Elizabeth Ray",
+            email: "arroyocolton@gmail.com",
+            active: true,
+            accounts: expect.arrayContaining([
+                371138, 
+                387979
+            ])
+        });
+    });
+
+    
+
 });
